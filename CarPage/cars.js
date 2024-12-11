747fd8c7e9e0a9f8f48ae9dd3d77c5fe25eda159
Eduard Erhart <edik.ergart2017@gmail.com>
2024-12-11 14:50:56 +0300

    Cars page. Added pages

---
+++ CarPage/cars.js
@@ -0,0 +1,32 @@
+const cars = [
+    {
+        brand: 'BMW',
+        model: 'BMW 3 Series',
+        modelYear: '2012'
+    },
+    {
+        brand: 'BMW',
+        model: 'BMW 5 Series',
+        modelYear: '2012'
+    },
+    {
+        brand: 'BMW',
+        model: 'BMW X5',
+        modelYear: '2013'
+    },
+    {
+        brand: 'Audi',
+        model: 'Audi A4',
+        modelYear: '2012'
+    },
+    {
+        brand: 'Audi',
+        model: 'Audi A5',
+        modelYear: '2013'
+    },
+    {
+        brand: 'Audi',
+        model: 'Audi A4',
+        modelYear: '2013'
+    },
+];
--- CarPage/main.js
+++ CarPage/main.js
@@ -1,5 +1,37 @@
 let contentHeaderTitle = document.getElementById('content-header-title');
+let menuItemCars = document.getElementById('menu-item-cars');
+let menuItemFavourite = document.getElementById('menu-item-favourite');
+let menuItemCompare = document.getElementById('menu-item-compare');
 
-function setContentHeaderTitle(title) {
+let welcomePage = document.getElementById('welcome-page');
+let carsPage = document.getElementById('cars-page');
+let favouritePage = document.getElementById('favourite-page');
+let comparePage = document.getElementById('compare-page');
+
+function selectPage(title, menuItemName) {
+    menuItemCars.classList.remove('active');
+    menuItemFavourite.classList.remove('active');
+    menuItemCompare.classList.remove('active');
+
+    welcomePage.classList.add('disp-none');
+    carsPage.classList.add('disp-none');
+    favouritePage.classList.add('disp-none');
+    comparePage.classList.add('disp-none');
+
+    switch(menuItemName) {
+        case 'menu-item-cars':
+            menuItemCars.classList.add('active');
+            carsPage.classList.remove('disp-none');            
+            break;
+        case 'menu-item-favourite':
+            menuItemFavourite.classList.add('active');
+            favouritePage.classList.remove('disp-none');
+            break;
+        case 'menu-item-compare':
+            menuItemCompare.classList.add('active');
+            comparePage.classList.remove('disp-none');
+            break;
+            
+    }
     contentHeaderTitle.textContent = title;
 }
